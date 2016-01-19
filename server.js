e528f5035c5255a64009848e65e309db1cb119d1
bridiver <github@brianjohnson.cc>
2016-01-19 13:37:46 -0700

    add cache settings for Fastly

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ server.register(require('inert'), err => {
     throw err
   }
 
+  server.ext('onPreResponse', function(request, reply) {
+    if (request.response.statusCode === 200) {
+      // send cache control headers for Fastly
+      // this will cache them at the fastly edge servers for a long
+      // period of time, but the browser clients will use the default
+      // cache-control settings and etags
+      request.response.headers['Surrogate-Control'] = 'max-age=2592000'
+    }
+    reply(request.response)
+  })
+
   // A server redirect to our favorite band, Brave Combo.
   server.route({
     method: 'GET',
