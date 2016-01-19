437faee076d9880108fae951617630a388470f84
Reuven Etzion <retzion@gmail.com>
2016-01-18 21:40:02 -0700

    mobile slide version

--- public/8675309_NBWS/assets/js/plugins/layer-slider.js
+++ public/8675309_NBWS/assets/js/plugins/layer-slider.js
@@ -11,7 +11,18 @@ var LayerSlider = function () {
                     pauseOnHover : true,
 			        responsiveUnder : 960,
 			        layersContainer : 960,
-                    hideUnder: 400,
+                    hideUnder: 480,
+                    navButtons: false,
+                    navStartStop: false,
+			        skinsPath: 'assets/plugins/layer-slider/layerslider/skins/'
+			    });
+		        jQuery("#layerslidermobile").layerSlider({
+			        skin: 'fullwidth',
+			        responsive : true,
+                    pauseOnHover : true,
+			        responsiveUnder : 960,
+			        layersContainer : 960,
+                    hideOver: 480,
                     navButtons: false,
                     navStartStop: false,
 			        skinsPath: 'assets/plugins/layer-slider/layerslider/skins/'
